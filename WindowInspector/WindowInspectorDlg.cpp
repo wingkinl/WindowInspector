e388bb65ec42f5cbf9067387820cf284c0026cd1
wingkinl <yonken@gmail.com>
2017-04-24 19:12:53 +0800

    getting started

--- WindowInspector/WindowInspectorDlg.cpp
+++ WindowInspector/WindowInspectorDlg.cpp
@@ -20,6 +20,7 @@ CWindowInspectorDlg::CWindowInspectorDlg(CWnd* pParent /*=NULL*/)
 	: CDialogEx(IDD_WINDOWINSPECTOR_DIALOG, pParent)
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+	m_nUpdateWndInfoTimer = 0;
 }
 
 void CWindowInspectorDlg::DoDataExchange(CDataExchange* pDX)
@@ -30,10 +31,18 @@ void CWindowInspectorDlg::DoDataExchange(CDataExchange* pDX)
 BEGIN_MESSAGE_MAP(CWindowInspectorDlg, CDialogEx)
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_WM_TIMER()
+	ON_WM_NCDESTROY()
 END_MESSAGE_MAP()
 
 
 // CWindowInspectorDlg message handlers
+
+enum
+{
+	TIMER_UPDATE_WIN_INFO_ID		= 100,
+	TIMER_UPDATE_WIN_INFO_ELAPSE	= 250,
+};
 
 BOOL CWindowInspectorDlg::OnInitDialog()
 {
@@ -44,7 +53,7 @@ BOOL CWindowInspectorDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
 
-	// TODO: Add extra initialization here
+	m_nUpdateWndInfoTimer = SetTimer(TIMER_UPDATE_WIN_INFO_ID, TIMER_UPDATE_WIN_INFO_ELAPSE, nullptr);
 
 	return TRUE;  // return TRUE  unless you set the focus to a control
 }
@@ -74,6 +83,9 @@ void CWindowInspectorDlg::OnPaint()
 	}
 	else
 	{
+		CPaintDC dc(this);
+		DrawWindowInfo(dc);
+
 		CDialogEx::OnPaint();
 	}
 }
@@ -85,3 +97,67 @@ HCURSOR CWindowInspectorDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+void CWindowInspectorDlg::DrawWindowInfo(CDC& dc)
+{
+	CPoint pos(0,0);
+	GetCursorPos(&pos);
+	HWND hWndInspect = ::WindowFromPoint(pos);
+	CRect rect;
+	GetClientRect(rect);
+	dc.FillSolidRect(rect, afxGlobalData.clrBtnFace);
+	if (!hWndInspect)
+		return;
+	m_hWndInspect = hWndInspect;
+	CWnd* pWndInspect = CWnd::FromHandle(m_hWndInspect);
+	auto pOldFont = dc.SelectObject(&GetGlobalData()->fontRegular);
+	
+	CString strLine, strText;
+	strLine.Format(_T("Pos: %d, %d"), pos.x, pos.y);
+	strText = strLine;
+
+	strLine.Format(_T("HWND: 0x%p"), m_hWndInspect);
+	strText += _T("\r\n");
+	strText += strLine;
+
+	pWndInspect->GetWindowText(strLine);
+	strText += _T("\r\nTitle: ");
+	strText += strLine;
+
+	const int nMaxClassLen = 256;
+	TCHAR szClassName[nMaxClassLen +1] = {0};
+	GetClassName(m_hWndInspect, szClassName, nMaxClassLen);
+	strText += _T("\r\nClass: ");
+	strText += szClassName;
+
+	HMODULE hModule = (HMODULE)GetClassLongPtr(m_hWndInspect, GCLP_HMODULE);
+	strLine.Format(_T("HMODULE: 0x%p"), hModule);
+	strText += _T("\r\n");
+	strText += strLine;
+
+
+	dc.DrawText(strText, rect, DT_NOPREFIX);
+
+	dc.SelectObject(pOldFont);
+}
+
+
+void CWindowInspectorDlg::OnTimer(UINT_PTR nIDEvent)
+{
+	if (nIDEvent == TIMER_UPDATE_WIN_INFO_ID)
+	{
+		Invalidate();
+		UpdateWindow();
+	}
+	CDialogEx::OnTimer(nIDEvent);
+}
+
+
+void CWindowInspectorDlg::OnNcDestroy()
+{
+	if (m_nUpdateWndInfoTimer)
+	{
+		KillTimer(m_nUpdateWndInfoTimer);
+		m_nUpdateWndInfoTimer = 0;
+	}
+	CDialogEx::OnNcDestroy();
+}
--- WindowInspector/WindowInspectorDlg.h
+++ WindowInspector/WindowInspectorDlg.h
@@ -19,15 +19,20 @@ public:
 
 	protected:
 	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
-
-
+protected:
+	void DrawWindowInfo(CDC& dc);
 // Implementation
 protected:
 	HICON m_hIcon;
+	UINT_PTR	m_nUpdateWndInfoTimer;
+	HWND		m_hWndInspect;
 
 	// Generated message map functions
 	virtual BOOL OnInitDialog();
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	afx_msg void OnNcDestroy();
 };
